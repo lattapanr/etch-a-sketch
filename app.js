7b969ae69c0a5f14f1917b8c92312be1b74c3e0d
Noey <noey.rachata@gmail.com>
2023-02-01 15:56:32 +0700

    - Change to use Flex to avoid unwanted whitespaces - Use range to change grid size - Add drawing function with mouse event - Clear the board by removing children

--- app.js
+++ app.js
@@ -2,15 +2,65 @@
 
 const container = document.querySelector('.container');
 const fragment = document.createDocumentFragment();
+const grid = document.querySelector('.grid');
+const btnReset = document.querySelector('.reset');
+const gridSize = document.querySelector('input');
+const labelRange = document.querySelector('label');
 
-const makeGrids = function (size = 16 * 16, className) {
-  for (let i = 0; i < size; i++) {
-    const grids = document.createElement('div');
-    fragment.appendChild(grids);
+function displaySize() {
+  labelRange.textContent = `${gridSize.value} x ${gridSize.value}`;
+}
+displaySize();
 
-    grids.classList.add('grid');
+function makeGrids(size) {
+  let boxSize = grid.offsetWidth / size;
+
+  for (let row = 0; row < size; row++) {
+    for (let col = 0; col < size; col++) {
+      const div = document.createElement('div');
+      fragment.appendChild(div);
+      div.classList.add('box');
+      div.style.width = `${boxSize}px`;
+      div.style.height = `${boxSize}px`;
+    }
   }
-};
+}
+//REMINDER: Have to create grids first before appending them to the 'container'
+makeGrids(gridSize.value);
+grid.appendChild(fragment);
 
-makeGrids();
-container.appendChild(fragment);
+function draw(e) {
+  /**
+   * Because dragging and selecting is the default behaviors on mousedown
+   * events, we needs to disable it first.
+   */
+  e.preventDefault();
+
+  /**
+   * This is for mouseover event when the user hold the mouse through the pixel.
+   */
+  if (e.buttons == 1)
+    this.style.backgroundColor = e.target.classList.add('active');
+}
+
+grid.addEventListener('mouseover', draw);
+
+//Change grid density
+gridSize.addEventListener('click', () => {
+  // if (grid.childElementCount > 0) {
+  //   grid.innerHTML = '';
+  // }
+  displaySize();
+  resetBoard();
+});
+
+//Remove the last child's if the first child's exist
+function resetBoard() {
+  while (grid.firstChild) {
+    grid.removeChild(grid.lastChild);
+  }
+  makeGrids(gridSize.value);
+  grid.appendChild(fragment);
+}
+
+btnReset.addEventListener('click', resetBoard);
