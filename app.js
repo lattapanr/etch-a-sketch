9f7ca4d2da04f1c9c3cd90bb598bd786d80d1a8f
Noey <noey.rachata@gmail.com>
2023-02-02 16:14:12 +0700

    Try to change color of pen

--- app.js
+++ app.js
@@ -6,53 +6,45 @@ const grid = document.querySelector('.grid');
 const btnReset = document.querySelector('.reset');
 const gridSize = document.querySelector('input');
 const labelRange = document.querySelector('label');
+const colorChoices = document.querySelectorAll('.option');
+const boxBackground = document.querySelectorAll('.active');
+let color = 'pink';
 
+//Functions
 function displaySize() {
-  labelRange.textContent = `${gridSize.value} x ${gridSize.value}`;
+  labelRange.textContent = `Grid density: ${gridSize.value} x ${gridSize.value}`;
 }
 displaySize();
 
+//Create cells and append it to DocumentFragment
 function makeGrids(size) {
-  let boxSize = grid.offsetWidth / size;
+  let boxSize = grid.clientWidth / size;
+  let gridSize = size * size;
 
-  for (let row = 0; row < size; row++) {
-    for (let col = 0; col < size; col++) {
-      const div = document.createElement('div');
-      fragment.appendChild(div);
-      div.classList.add('box');
-      div.style.width = `${boxSize}px`;
-      div.style.height = `${boxSize}px`;
-    }
+  grid.classList.remove('active');
+
+  for (let i = 0; i < gridSize; i++) {
+    const div = document.createElement('div');
+    div.classList.add('box');
+    div.style.width = `${boxSize}px`;
+    div.style.height = `${boxSize}px`;
+    fragment.appendChild(div);
   }
 }
 //REMINDER: Have to create grids first before appending them to the 'container'
 makeGrids(gridSize.value);
 grid.appendChild(fragment);
 
+//Activates mouse to draw when the user keeps holding the mouse down through the boxes
 function draw(e) {
-  /**
-   * Because dragging and selecting is the default behaviors on mousedown
-   * events, we needs to disable it first.
-   */
   e.preventDefault();
-
-  /**
-   * This is for mouseover event when the user hold the mouse through the pixel.
-   */
-  if (e.buttons == 1)
+  if (e.buttons == 1) {
+    //Only activate the mouse event when ONE button is being clicked/held
     this.style.backgroundColor = e.target.classList.add('active');
+    //TODO: check to see what the difference between these two. the below seems to cause the whole board to change color when dragging is being held
+    // e.target.style.backgroundColor = color;
+  }
 }
-
-grid.addEventListener('mouseover', draw);
-
-//Change grid density
-gridSize.addEventListener('click', () => {
-  // if (grid.childElementCount > 0) {
-  //   grid.innerHTML = '';
-  // }
-  displaySize();
-  resetBoard();
-});
 
 //Remove the last child's if the first child's exist
 function resetBoard() {
@@ -63,4 +55,67 @@ function resetBoard() {
   grid.appendChild(fragment);
 }
 
+//Change colors
+function changePenColor() {
+  colorChoices.forEach(choice => {
+    choice.addEventListener('click', getTheme);
+  });
+}
+changePenColor();
+
+function getTheme(e) {
+  if (e.target.id === 'winter') {
+    console.log(getWinter());
+  } else if (e.target.id === 'spring') {
+    return getSpring();
+  } else if (e.target.id === 'summer') {
+    return getSummer();
+  } else if (e.target.id === 'autumn') {
+    return getAutumn();
+  }
+}
+
+function getWinter() {
+  const winter = ['#bee9e8', '#62b6cb', '#62b6cb', '#cae9ff', '#5fa8d3'];
+  let color = winter[Math.floor(Math.random() * winter.length)];
+
+  return color;
+}
+
+function getSpring() {
+  const spring = ['#ff8ba0', '#ffa5a1', '#ffc2a6', '#f9de87', '#fce9c4'];
+  let color = winter[Math.floor(Math.random() * spring.length)];
+
+  return color;
+}
+
+function getSummer() {
+  const summer = ['#fabc3c', '#ffb238', '#f19143', '#ff773d', '#f55536'];
+  let color = winter[Math.floor(Math.random() * summer.length)];
+
+  return color;
+}
+
+function getAutumn() {
+  const autumn = ['#6f1d1b', '#bb9457', '#432818', '#99582a', '#ffe6a7'];
+
+  let color = winter[Math.floor(Math.random() * autumn.length)];
+
+  return color;
+}
+
+//Event Listeners
+
+//Drawing event
+grid.addEventListener('mouseover', draw);
+
+//Change grid density
+gridSize.addEventListener('click', () => {
+  displaySize();
+  resetBoard();
+});
+
+//Clear button
 btnReset.addEventListener('click', resetBoard);
+
+// TODO: change the color of the pen
