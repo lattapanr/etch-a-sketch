eff77795b8daad493a0ca2ddaf6e9a9cf753dfb7
Noey <noey.rachata@gmail.com>
2023-02-09 14:16:35 +0700

    Refactor code

--- app.js
+++ app.js
@@ -8,16 +8,10 @@ const gridSize = document.querySelector('input');
 const labelRange = document.querySelector('label');
 const colorPicker = document.querySelector('#color-picker');
 let color = 'black';
-let click = true;
-const bgColor = `rgba(255, 255, 255, 0.651)`;
+let click = false;
 
 //Functions
-function displaySize() {
-  labelRange.textContent = `Grid density: ${gridSize.value} x ${gridSize.value}`;
-}
-displaySize();
-
-//Create cells and append it to DocumentFragment
+//Create cells and append it to the document
 function makeGrids(size) {
   grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
   grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
@@ -30,65 +24,58 @@ function makeGrids(size) {
     grid.appendChild(divs);
   }
 }
-//REMINDER: Have to create grids first before appending them to the 'container'
 makeGrids(gridSize.value);
 
-//Activates mouse to draw when the user keeps holding the mouse down through the boxes
-function draw(e) {
-  e.preventDefault();
-  if (click) {
-    if (color === 'winter') {
-      this.style.backgroundColor = getWinter();
-    } else if (color === 'spring') {
-      this.style.backgroundColor = getSpring();
-    } else if (color === 'summer') {
-      this.style.backgroundColor = getSummer();
-    } else if (color === 'autumn') {
-      this.style.backgroundColor = getAutumn();
-    }
-  } else if (color === 'color') {
-    this.style.backgroundColor = pickColor();
-  } else {
-    this.style.backgroundColor = color;
-  }
+function displaySize() {
+  labelRange.textContent = `Grid density: ${gridSize.value} x ${gridSize.value}`;
+}
+displaySize();
+
+//Arrays of random colors by seasons theme
+const seasons = {
+  winter: ['#bee9e8', '#62b6cb', '#62b6cb', '#cae9ff', '#5fa8d3'],
+  spring: ['#ff8ba0', '#ffa5a1', '#ffc2a6', '#f9de87', '#fce9c4'],
+  summer: ['#fabc3c', '#ffb238', '#f19143', '#ff773d', '#f55536'],
+  autumn: ['#b39c4d', '#373d20', '#717744', '#bcbd8b', '#766153'],
+};
+
+//Get random colors from 'seasons object and return colors of each season that is called in the parameter 'season'
+function getRandomColor(season) {
+  const colors = seasons[season];
+  return colors[Math.floor(Math.random() * colors.length)];
 }
 
-//Change colors
-function changePenColor(colorChoice) {
-  color = colorChoice;
-}
-
+//Get value from the color picker
 function pickColor() {
   return colorPicker.value;
 }
 
-function getWinter() {
-  const winter = ['#bee9e8', '#62b6cb', '#62b6cb', '#cae9ff', '#5fa8d3'];
-  let color = winter[Math.floor(Math.random() * winter.length)];
-
-  return color;
+//Change colors of the pen when the button of each color choice is clicked (HTML's onClick)
+function changePenColor(colorChoice) {
+  color = colorChoice;
 }
 
-function getSpring() {
-  const spring = ['#ff8ba0', '#ffa5a1', '#ffc2a6', '#f9de87', '#fce9c4'];
-  let color = spring[Math.floor(Math.random() * spring.length)];
+//Activates mouse to draw when 'click' is truthy with assigned color
+function draw(e) {
+  e.preventDefault();
+  //if click is falsy, the conditions wont run
+  if (!click) return;
+  let bgColor;
 
-  return color;
-}
-
-function getSummer() {
-  const summer = ['#fabc3c', '#ffb238', '#f19143', '#ff773d', '#f55536'];
-  let color = summer[Math.floor(Math.random() * summer.length)];
-
-  return color;
-}
-
-function getAutumn() {
-  const autumn = ['#b39c4d', '#373d20', '#717744', '#bcbd8b', '#766153'];
-
-  let color = autumn[Math.floor(Math.random() * autumn.length)];
-
-  return color;
+  switch (color) {
+    case 'winter':
+    case 'spring':
+    case 'summer':
+    case 'autumn':
+      bgColor = getRandomColor(color);
+      break;
+    case 'color':
+      bgColor = pickColor();
+      break;
+    default:
+      bgColor = color;
+  }
+  this.style.backgroundColor = bgColor;
 }
 
 //Remove the last child's if the first child's exist
@@ -97,12 +84,10 @@ function resetBoard() {
     grid.removeChild(grid.lastChild);
   }
   makeGrids(gridSize.value);
-  color = 'black';
 }
 
 //Event Listeners
-
-//Drawing event
+//Toggling the click event on the grid canvas. The draw function only operates when 'click' is true
 grid.addEventListener('click', () => (click = !click));
 
 //Change grid density
@@ -111,5 +96,4 @@ gridSize.addEventListener('click', () => {
   resetBoard();
 });
 
-//Clear button
 btnReset.addEventListener('click', resetBoard);
